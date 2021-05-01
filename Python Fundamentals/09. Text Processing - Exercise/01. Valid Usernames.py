b96f57aa519a0ddbae23b2948322cb84e65f0cae
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2021-05-01 18:26:56 +0200

    Delete 01. Valid Usernames.py

--- Python Fundamentals/09. Text Processing - Exercise/01. Valid Usernames.py
+++
@@ -1,6 +0,0 @@
-characters = input().split(", ")
-
-for username in characters:
-    if 3 <= len(username) <= 16:
-        if username.isalnum() or "-" in username or "_" in username:
-            print(username)
