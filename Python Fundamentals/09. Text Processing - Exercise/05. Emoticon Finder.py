eff272a9085c516e1fb02190466f106bf30f5220
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2021-05-01 18:27:07 +0200

    Delete 05. Emoticon Finder.py

--- Python Fundamentals/09. Text Processing - Exercise/05. Emoticon Finder.py
+++
@@ -1,9 +0,0 @@
-text = input()
-symbol = ":"
-
-result = []
-
-for index in range(len(text)):
-    if text[index] == symbol:
-        result.append(f"{symbol}{text[index+1]}")
-print("\n".join(result))
