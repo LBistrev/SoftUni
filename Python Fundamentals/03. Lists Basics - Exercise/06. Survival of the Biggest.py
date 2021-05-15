6f996b998bbdc62f21175f7f0531244120c3ef25
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2021-05-15 13:24:50 +0200

    Update 06. Survival of the Biggest.py

--- Python Fundamentals/03. Lists Basics - Exercise/06. Survival of the Biggest.py
+++ Python Fundamentals/03. Lists Basics - Exercise/06. Survival of the Biggest.py
@@ -1,14 +1,14 @@
 list_of_integer = input().split()
 numbers_to_remove = int(input())
 
-list_of_biggest = []
+list_of_biggest = [int(num) for num in list_of_integer]
 
-for numbers in list_of_integer:
-    number = int(numbers)
-    list_of_biggest.append(number)
+# for numbers in list_of_integer:
+#     number = int(numbers)
+#     list_of_biggest.append(number)
 
 for smallest in range(numbers_to_remove):
     min_number = min(list_of_biggest)
     list_of_biggest.remove(min_number)
 
-print(list_of_biggest)
+print(', '.join([str(num) for num in list_of_biggest]))
