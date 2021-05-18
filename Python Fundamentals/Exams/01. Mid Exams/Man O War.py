44c6dc835c1d22ed69ea92c3f94c4452a6e7135a
Lyubomir Bistrev <lubo_bistrev@abv.bg>
2021-05-18 21:16:35 +0200

    Update Man O War.py

--- Python Fundamentals/Exams/01. Mid Exams/Man O War.py
+++ Python Fundamentals/Exams/01. Mid Exams/Man O War.py
@@ -1,50 +1,53 @@
-status_pirate_ship = input().split(">")
-status_battle_ship = input().split(">")
+def is_index_valid(current_index, current_data):
+    if current_index in range(len(current_data)):
+        return True
+    return False
+
+
+status_pirate_ship = [int(el) for el in input().split(">")]
+status_warship = [int(el) for el in input().split(">")]
 max_health = int(input())
-command = input()
-pirate_ship_integers = [int(n) for n in status_pirate_ship]
-battle_ship_integers = [int(n) for n in status_battle_ship]
-is_valid = True
-while not command == "Retire":
-    data = command.split()
+is_sink = False
+commands = input()
+while not commands == "Retire":
+    data = commands.split()
     if data[0] == "Fire":
         index = int(data[1])
         damage = int(data[2])
-        if 0 <= index < len(status_battle_ship):
-            battle_ship_integers[index] -= damage
-            if battle_ship_integers[index] <= 0:
-                print(f"You won! The enemy ship has sunken.")
-                is_valid = False
+        if is_index_valid(index, status_warship):
+            status_warship[index] -= damage
+            if status_warship[index] <= 0:
+                print("You won! The enemy ship has sunken.")
+                is_sink = True
                 break
     elif data[0] == "Defend":
         start_index = int(data[1])
         end_index = int(data[2])
         damage = int(data[3])
-        if 0 <= start_index < len(pirate_ship_integers) and 0 <= end_index < len(pirate_ship_integers):
-            for el in range(start_index, end_index+1):
-                pirate_ship_integers[el] -= damage
-                if pirate_ship_integers[el] <= 0:
+        if is_index_valid(start_index, status_pirate_ship) and is_index_valid(end_index, status_pirate_ship):
+            for attack in range(start_index, end_index+1):
+                status_pirate_ship[attack] -= damage
+                if status_pirate_ship[attack] <= 0:
                     print("You lost! The pirate ship has sunken.")
-                    is_valid = False
+                    is_sink = True
                     break
     elif data[0] == "Repair":
         index = int(data[1])
         health = int(data[2])
-        if 0 <= index < len(pirate_ship_integers):
-            if pirate_ship_integers[index] + health > max_health:
-                pirate_ship_integers[index] = max_health
+        if is_index_valid(index, status_pirate_ship):
+            if status_pirate_ship[index] + health > max_health:
+                status_pirate_ship[index] = max_health
             else:
-                pirate_ship_integers[index] += health
-
+                status_pirate_ship[index] += health
     elif data[0] == "Status":
-        count = 0
-        for section in range(len(pirate_ship_integers)):
-            if pirate_ship_integers[section] < max_health * 0.20:
-                count += 1
-        print(f"{count} sections need repair.")
+        need_repair = 0
+        for el in status_pirate_ship:
+            if el < max_health * 0.20:
+                need_repair += 1
+        print(f"{need_repair} sections need repair.")
 
-    command = input()
-
-if is_valid:
-    print(f"Pirate ship status: {sum(pirate_ship_integers)}")
-    print(f"Warship status: {sum(battle_ship_integers)}")
+    commands = input()
+if not is_sink:
+    if sum(status_pirate_ship) and sum(status_warship):
+        print(f"Pirate ship status: {sum(status_pirate_ship)}")
+        print(f"Warship status: {sum(status_warship)}")
