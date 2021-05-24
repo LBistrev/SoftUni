d60c5889504377dc78b05085d9bdcbd7de70d504
Lyubomir Bistrev <lubo_bistrev@abv.bg>
2021-05-24 21:33:07 +0200

    Delete 01. Reverse Numbers with a Stack.py

--- Python Advanced/01. Reverse Numbers with a Stack.py
+++
@@ -1,8 +0,0 @@
-numbers = [int(num) for num in input().split()]
-
-stack = []
-while numbers:
-    current_num = numbers.pop()
-    stack.append(current_num)
-
-print(" ".join([str(el) for el in stack]))
