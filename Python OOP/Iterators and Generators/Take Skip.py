6a0148eb87973c012a12aa05b78c5dcffdd5ccdc
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2021-08-28 15:25:51 +0200

    Create Take Skip.py

---
+++ Python OOP/Iterators and Generators/Take Skip.py
@@ -0,0 +1,17 @@
+class take_skip:
+    def __init__(self, step, count):
+        self.step = step
+        self.count = count
+        self.start = 0
+        self.counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.counter >= self.count:
+            raise StopIteration
+        self.counter += 1
+        temp = self.start
+        self.start += self.step
+        return temp
