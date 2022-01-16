9a293eecb0773943e260591a66bb2f97888c765c
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2022-01-16 15:23:50 +0100

    Create HTML Tags.py

---
+++ Python OOP/Decorators/HTML Tags.py
@@ -0,0 +1,9 @@
+def tags(tag):
+    def decorator(function):
+        def wrapper(*args):
+            result = function(*args)
+
+            return f"<{tag}>{result}</{tag}>"
+
+        return wrapper
+    return decorator
